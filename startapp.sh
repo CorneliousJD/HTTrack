1d77af82523b3e3e770f5f041276ba2d92844fbb
CorneliousJD <corneliousjd@gmail.com>
2025-02-02 21:05:35 -0500

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
 
-# Start HTTrack (if applicable)
-httrack --update --quiet &
-
-# Start the VNC server in the background
-x11vnc -forever -usepw -create &
-
-# Start Openbox session in the background
-openbox-session &
-
-# Wait forever to keep the container alive
-tail -f /dev/null
