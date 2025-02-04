85ac687970cd5b173e48579749c51813f7d5431e
CorneliousJD <corneliousjd@gmail.com>
2025-02-03 19:42:24 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,18 +6,14 @@ Xvfb :1 -screen 0 1280x960x16 &
 # Set the DISPLAY environment variable
 export DISPLAY=:1
 
-# Start D-Bus
-mkdir -p /var/run/dbus
-dbus-daemon --system --fork
-
 # Start fluxbox (window manager)
 fluxbox &
 
 # Start x11vnc (VNC server) with clipboard support
 x11vnc -display :1 -forever -shared -rfbport 5901 -clipboard &
 
-# Start noVNC (web-based VNC client) on port 5800 with clipboard support
-/opt/novnc/utils/launch.sh --vnc localhost:5901 --listen 5800 --web /opt/novnc &
+# Start noVNC (web-based VNC client) on port 5800
+/opt/novnc/utils/launch.sh --vnc localhost:5901 --listen 5800 &
 
 # Wait for Fluxbox to initialize
 sleep 2
@@ -25,6 +21,9 @@ sleep 2
 # Start autocutsel to synchronize the clipboard
 autocutsel -selection PRIMARY -fork &
 autocutsel -selection CLIPBOARD -fork &
+
+# Start xfce4-clipman to manage the clipboard
+xfce4-clipman &
 
 # Start WebHTTrack (GUI version) in the Fluxbox environment
 webhttrack &
